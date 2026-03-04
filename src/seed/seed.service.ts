f7fe29247c479d923837b686d89a96b709388f9f
Gabosor <z.kdeqth1@gmail.com>
2026-03-03 21:44:58 -0400

    Se termino la seccion de seed.

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
+  ) { }
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany();
+    const pokemonToInsert: { name: string, no: number }[] = [];
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      pokemonToInsert.push({ name, no });
+    })
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed Executed';
+  }
+}
